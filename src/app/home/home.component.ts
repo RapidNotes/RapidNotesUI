9f1c7de454d11c5c0fae0a67865774edacde2519
shashwat pragya <shashwat183@gmail.com>
2021-01-09 21:17:04 +0000

    NA-1 Remove ckeditor code and implement ngx-quill

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import * as BalloonEditor from '@ckeditor/ckeditor5-build-balloon';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +7,6 @@ import * as BalloonEditor from '@ckeditor/ckeditor5-build-balloon';
 })
 export class HomeComponent implements OnInit {
 
-  public Editor = BalloonEditor;
   public editorConfig = {}
   public listOfNotes = [
     'Shopping List',
